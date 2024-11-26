d98ea34772e6813633127fea5cc37a3b7dee7922
Daniil Lyasman <danlaysma45.work@gmail.com>
2024-11-26 17:08:16 +0600

    [*] change table test case to have it id and insert values for layer and automatation

---
+++ src/main/resources/db/migration/V1_3__init.sql
@@ -0,0 +1,5 @@
+INSERT INTO layer(layer_name) values('E2E');
+INSERT INTO layer(layer_name) values('API');
+INSERT INTO layer(layer_name) values('UNIT');
+INSERT INTO automation_status(automation_name) values('Manual');
+INSERT INTO automation_status(automation_name) values('Automated');
