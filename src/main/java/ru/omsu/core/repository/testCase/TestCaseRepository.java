07a1604e1a802468b328eb0d6b1f0083cfa0c4d3
Daniil Lyasman <danlaysma45.work@gmail.com>
2024-11-26 16:37:42 +0600

    [*] edit request to get test case

--- src/main/java/ru/omsu/core/repository/testCase/TestCaseRepository.java
+++ src/main/java/ru/omsu/core/repository/testCase/TestCaseRepository.java
@@ -17,14 +17,16 @@ public class TestCaseRepository implements ITestCaseRepository {
 
     @Override
     public UUID addTestCase(TestCaseRequest testCaseRequest) {
-        return jdbcOperations.queryForObject("INSERT INTO test_case(test_case_id,test_case_name,suite_id,automation_status,layer) VALUES (?,?,?,?,?) RETURNING test_case_id",
+        return jdbcOperations.queryForObject("INSERT INTO test_case(test_case_id,test_case_name,suite_id,automation_status_id,layer_id) VALUES (?,?,?,?,?) RETURNING test_case_id",
                 (resultSet, i) -> UUID.fromString(resultSet.getString("test_case_id")),
                 testCaseRequest.getTestCaseName(), testCaseRequest.getSuiteId(), testCaseRequest.getIsAutomatedId(), testCaseRequest.getLayerId());
     }
 
     @Override
     public TestCase getTestCase(UUID testCaseId) {
-        return jdbcOperations.queryForObject("SELECT test_case_id,test_case_name,suite_id,automation_status,layer from test_case where test_case_id=?",
+        return jdbcOperations.queryForObject("SELECT test_case_id,test_case_name,suite_id,automation_status_name,layer_name from test_case where test_case_id=? " +
+                        "LEFT JOIN layer ON layer.layer_id =test_case.layer_id " +
+                        "LEFT JOIN automation_status ON automation_status.automation_status_id=test_case.automation_status_id",
                 (resultSet, i) -> new TestCase(
                         UUID.fromString(resultSet.getString("test_case_id")),
                         resultSet.getString("test_case_name"),
